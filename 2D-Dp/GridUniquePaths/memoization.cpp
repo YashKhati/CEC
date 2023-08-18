44c3381597924d1b16f799e5b34b9414ab1f205e
yash <khatiyash010703@gmail.com>
2023-08-18 20:18:09 +0530

    memoization updated

--- 2D-Dp/GridUniquePaths/memoization.cpp
+++ 2D-Dp/GridUniquePaths/memoization.cpp
@@ -4,7 +4,7 @@ https://www.codingninjas.com/studio/problems/unique-paths_1081470?utm_source=str
 #include <iostream>
 #include <vector>
 using namespace std;
-int help(vector<vector<int>> &dp, int m, int n, int i, int j)
+int help(vector<vector<int>> &dp, int i, int j)
 {
 
     if (i < 0 || j < 0)
@@ -13,13 +13,13 @@ int help(vector<vector<int>> &dp, int m, int n, int i, int j)
     }
     if (i == 0 && j == 0)
     {
-        return dp[i][j] = 0;
+        return dp[i][j] = 1;
     }
     if (dp[i][j] != -1)
     {
         return dp[i][j];
     }
-    return dp[i][j] = help(dp, m, n, i - 1, j) + help(dp, m, n, i, j - 1);
+    return dp[i][j] = help(dp, i - 1, j) + help(dp, i, j - 1);
 }
 int main()
 {
@@ -27,5 +27,5 @@ int main()
     cout << "Enter value of M and N  : ";
     cin >> m >> n;
     vector<vector<int>> vec(m, vector<int>(n, -1));
-    cout << "Unique Path : " << help(vec, m - 1, n - 1, m - 1, n - 1);
+    cout << "Unique Path : " << help(vec, m - 1, n - 1);
 }
